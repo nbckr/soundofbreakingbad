efd81859bf9ef8c56437cb4c82dc52342e13e523
Niels Boecker <niels.boecker@maibornwolff.de>
2016-07-21 19:40:36 +0200

    Experiment with SVG ... inline is the best way to style with CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,7 @@ module.exports = function (grunt) {
                         "**/*.css",
                         "**/*.scss",
                         "**/*.js",
+                        "**/*.svg",
                         "**/*.html"
                     ]
                 },
